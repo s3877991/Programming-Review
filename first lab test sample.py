f3bd3b32b411f4328687cca602ee662d84dc1c33
Nguyễn Quang Duy <s3877991@rmit.edu.vn>
2021-01-09 10:47:00 +0700

    sample modified

--- first lab test sample.py
+++ first lab test sample.py
@@ -21,21 +21,21 @@ def options():
 
 def find_top3(numbers):
     """Convert a number string to a number list , sort all elements and find 3 largest numbers from that list"""
-    number_character_list = numbers.split()
+    number_string_list = numbers.split()
     number_list = []
     sorted_number_list = []
-    for string_element in number_character_list:
+    for string_element in number_string_list:        # Convert a number string element to integer in a list
         number_element = int(string_element)
         number_list.append(number_element)
-    while number_list:
+    while number_list:                              # Sort the element from minimum to maximum values
         minimum = number_list[0]
         for element in number_list:
             if element < minimum:
                 minimum = element
         sorted_number_list.append(minimum)
         number_list.remove(minimum)
-    max_index = len(sorted_number_list)
-    top_3_list = sorted_number_list[max_index - 3: max_index + 1]
+    max_index = len(sorted_number_list)                                     # Find 3 maximum values at the end of the
+    top_3_list = sorted_number_list[max_index - 3: max_index + 1]           # sorted list
     return top_3_list
 
 
@@ -65,7 +65,7 @@ def outcome():
         duy.goto(-400, 0)
         duy.down()
         for side in range(3, 11):
-            multiple_color = color * side
+            multiple_color = color * side           # There are 8 shapes to color but there are 4 colors to fill shapes
             draw_polygons(duy, side, multiple_color[index])
             index += 1
         win.exitonclick()

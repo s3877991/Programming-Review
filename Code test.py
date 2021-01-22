a241e7be64ee1f5ce82299e7beed43baf90edf1e
Nguyễn Quang Duy <s3877991@rmit.edu.vn>
2021-01-22 11:46:20 +0700

    Review for final lab test - Update #3

---
+++ Code test.py
@@ -0,0 +1,116 @@
+# import turtle
+
+
+# def draw_polygons(turtle, side, color):
+#    """Draw polygons with turtle and color them with color"""
+#    turtle.fillcolor(color)
+#    turtle.begin_fill()
+#    for point in range(side + 1):
+#        turtle.forward(100)
+#        turtle.left(180 * 2 / side)
+#    turtle.end_fill()
+#    turtle.setheading(0)
+
+
+# win = turtle.Screen()
+# duy = turtle.Turtle()
+# color = ['red', 'yellow', 'green', 'blue']
+# duy.up()
+# duy.goto(-400, 0)
+# duy.down()
+# index = 0
+# for side in range(3, 11):
+#    double_color = color * side
+#    draw_polygons(duy, side, double_color[index])
+#    index += 1
+
+# win.exitonclick()
+
+
+# def find_top_3(numbers):
+#    number_character_list = numbers.split()
+#    list = []
+#    sorted_list = []
+#    for string_element in number_character_list:
+#        number_element = int(string_element)
+#        list.append(number_element)
+#    while list:
+#        min = list[0]
+#        for phan_tu in list:
+#            if phan_tu < min:
+#                min = phan_tu
+#        sorted_list.append(min)
+#        list.remove(min)
+#    max_index = len(sorted_list)
+#    top_3_list = sorted_list[max_index - 3: max_index + 1]
+#    return top_3_list
+
+
+# numbers = '1 3 5 6 2 4 5 7'
+# print(find_top_3(numbers)))
+
+
+# def second_largest(numbers):
+#    idx = -1
+#    while numbers:
+#        if numbers[idx] == numbers[idx - 1]:
+#            idx += -1
+#        elif numbers[idx] > numbers[idx - 1]:
+#            return numbers[idx - 1]
+
+
+# print(second_largest([1, 2, 2, 3, 4, 4]))
+# print(second_largest([1, 2, 3, 4]))
+
+
+# def read_inventory(file_name):
+#    f = open(file_name, 'r')
+#    products = {}
+#    for line in f:
+#        words = line.split()
+#        product_name = words[0]
+#        quantity = int(words[1])
+#        if product_name in products:
+#            products[product_name] = products[product_name] + quantity
+#        else:
+#            products[product_name] = quantity
+#    f.close()
+#    return products
+
+
+# def display_inventory(products):
+#    for product_name, quantity in products.items():
+#        print(product_name, "has", quantity, "items")
+
+
+# my_products = read_inventory('inventory.txt')
+# display_inventory(my_products)
+
+# res = []
+# for number in range(1000, 3001):
+#    string = str(number)
+#    if (int(string[0]) % 2 == 0) and (int(string[1]) % 2 == 0) and (int(string[2]) % 2 == 0) and (
+#            int(string[3]) % 2 == 0):
+#        res.append(string)
+# print(','.join(res))
+
+
+# def substitution_cipher(message, key_to_encrypt):
+#    alphabet_string = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+#    encrypted_message = ''
+#    alphabet_list = list(alphabet_string)
+#    key_list = list(key_to_encrypt)
+#    encrypt = {}
+#    for key in alphabet_list:
+#        for value in key_list:
+#            encrypt[key] = value
+#            key_list.remove(value)
+#            break
+#    for char in message:
+#        if char not in alphabet_string:
+#            encrypted_message += char
+#        else:
+#            encrypted_message += encrypt[char]
+#    return encrypted_message
+
+# print(substitution_cipher('HELLO WORLD', 'QTGABCDEFHIJKLMNOPRSUVXYZ'))
--- week 11.py
+++ week 11.py
@@ -1,54 +1,81 @@
 # TUTORIAL WEEK 11: Putting things together.
 
 # EXERCISE 1:
-res = []                            # initial result list
-for number in range(1000, 3001):
-    string = str(number)            # convert an integer into a string
-    # if all digits in an integer are even numbers the numbers meeting the constraint will be printed
-    # in a comma-separated sequence on a single line.
-    if int(string[0]) % 2 == 0 and int(string[1]) % 2 == 0 and int(string[2]) % 2 == 0 and int(string[3]) % 2 == 0:
-        res.append(string)
-print(','.join(res))
+def even_number_digit(first_number, last_number):
+    res = []                                                # initial result list
+    for number in range(first_number, last_number + 1):
+        string = str(number)                                # convert an integer into a string
+        # if all digits in an integer are even numbers the numbers meeting the constraint will be printed
+        # in a comma-separated sequence on a single line.
+        if int(string[0]) % 2 == 0 and int(string[1]) % 2 == 0 and int(string[2]) % 2 == 0 and int(string[3]) % 2 == 0:
+            res.append(string)
+    return ', '.join(res)
+
+
+print(even_number_digit(1000, 3000))        # OUTPUT: 2000, 2002, 2004,... 2884, 2886, 2888
 
 
 # EXERCISE 2:
 # Way 1: Use for loop
 def factorial_iteration(n):
     """Compute factorial of an integer n using iteration"""
-    res = 1                         # initial value
+    res = 1                                                 # initial value
     for i in range(1, n + 1):
         res = res * i
-    return res                      # final value
+    return res                                              # final value
 
 
-print(factorial_iteration(4))
+print(factorial_iteration(4))       # OUTPUT: 24
 
 
 # Way 2: Use recursion
 def factorial_recursion(n):
-    """
-    compute factorial of an integer n using recursion
-    """
-    if n == 1:                                          # base part
+    """Compute factorial of an integer n using recursion"""
+    if n == 1:                                              # base part
         return 1
-    return n * factorial_recursion(n - 1)               # recursion part
+    return n * factorial_recursion(n - 1)                   # recursion part
 
 
-print(factorial_recursion(4))
+print(factorial_recursion(4))       # OUTPUT: 24
 
 
 # EXERCISE 3:
+def substitution_cipher(message, key_to_encrypt):
+    """From the plain message and key, encrypt that message with that key"""
+    alphabet_string = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'          # create a string of original alphabet
+    encrypted_message = ''                                  # initial result of the encrypted message string
+    alphabet_list = list(alphabet_string)                   # convert two character strings into two separate list
+    key_list = list(key_to_encrypt)
+    encrypt = {}                                            # from those two list, create a dictionary by using
+    for key in alphabet_list:                               # Naive methods
+        for value in key_list:                              # key: original alphabet letters
+            encrypt[key] = value                            # value: encrypted alphabet letter
+            key_list.remove(value)
+            break
+    for char in message:                                    # from a plain message string and the dictionary created
+        if char not in alphabet_string:                     # recently, encrypted that message and return the
+            encrypted_message += char                       # encrypted one
+        else:
+            encrypted_message += encrypt[char]
+    return encrypted_message
+
+
+print(substitution_cipher('HELLO WORLD', 'QTGABCDEFHIJKLMNOPRSUVXYZ'))      # OUTPUT: EBJJM XMPJA
+
+
+# EXERCISE 4:
 
 
 
 # EXERCISE 6:
 def is_password_strong_enough(password):
+    """If password meets some certain requirements, the function returns True. Otherwise, it returns False"""
     character_string = '!@#$%^&*'
     upper_case_valid = False
     lower_case_valid = False
     number_valid = False
     character_valid = False
-    if 6 <= len(password) <= 12:                        # Check the length of password
+    if 6 <= len(password) <= 12:                            # Check the length of password
         # Check all characters in a password:
         # at least 1 upper-case letter, 1 lower-case letter, 1 number and 1 special character
         for char in password:
@@ -64,7 +91,7 @@ def is_password_strong_enough(password):
     return False
 
 
-print(is_password_strong_enough('Aa12!@'))
-print(is_password_strong_enough('ABC123'))
-print(is_password_strong_enough('ABC123abc'))
-print(is_password_strong_enough('ABC123abc!@#'))
+print(is_password_strong_enough('Aa12!@'))                  # OUTPUT: True
+print(is_password_strong_enough('ABC123'))                  # OUTPUT: False
+print(is_password_strong_enough('ABC123abc'))               # OUTPUT: False
+print(is_password_strong_enough('ABC123abc!@#'))            # OUTPUT: True
